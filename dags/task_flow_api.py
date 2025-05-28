ca77d29034fef94851f704330797b08e09844ab8
SaberHardy <samirsamado5@gmail.com>
2025-05-28 17:57:52 +0100

    add tasks using airflow APIs @task

---
+++ dags/task_flow_api.py
@@ -0,0 +1,48 @@
+from airflow.sdk import dag, task
+import pendulum
+import json
+
+
+@dag(schedule=None, start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+     catchup=False, tags=["example"])
+def tutorial_taskflow_api():
+    @task()
+    def extract():
+        """
+        A simple Extract task to get data ready for the rest of the data pipeline.
+        In this case, getting data is simulated by reading from a hardcoded JSON string.
+        """
+        data_string = '{"1001": 301.27, "1002": 433.21, "1003": 502.22}'
+
+        order_data_dict = json.loads(data_string)
+        print(f"The ordred data is: {order_data_dict}")
+        return order_data_dict
+
+    @task(multiple_outputs=True)
+    def transform(order_data_dict: dict):
+        """
+        A simple Transform task which takes in the collection of order data and computes the total order value.
+        """
+        total_order_value = 0
+
+        for value in order_data_dict.values():
+            total_order_value += value
+
+        print(f"The transformed data: {total_order_value}")
+        return {"total_order_value": total_order_value}
+
+    @task()
+    def load(total_order_value: float):
+        """
+        A simple Load task which takes in the result of the Transform task and
+        instead of saving it to end user review, just prints it out.
+        """
+
+        print(f"Total order value is: {total_order_value:.2f}")
+
+    order_data = extract()
+    order_summary = transform(order_data)
+    load(order_summary["total_order_value"])
+
+
+tutorial_taskflow_api()
